22d34eaf6c3383ff18c2385e9569c2f78b3df303
jules2pommes <jules.jag.28@gmail.com>
2025-11-04 19:40:55 +0100

    TP2.2 invariant pre post conditions

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -7,6 +7,7 @@ public class Romain {
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
+		assert isInvariantVerified();
 	}
 
 	public String getNom() {
@@ -22,16 +23,29 @@ public class Romain {
 	}
 
 	public void recevoirCoup(int forceCoup) {
+		assert isInvariantVerified();
+		int buffer = force;
 		force -= forceCoup;
 		if (force < 1) {
 			parler("J'abandonne !");
 		} else {
 			parler("Aïe");
 		}
+		assert isInvariantVerified();
+		assert force - buffer < 0;
+	}
+	
+	private boolean isInvariantVerified() {
+		return force >= 0;
 	}
 
 	@Override
 	public String toString() {
 		return nom;
+	}
+	
+	public static void main(String[] args) {
+		Romain minus = new Romain("Minus", 6);
+		
 	}
 }
